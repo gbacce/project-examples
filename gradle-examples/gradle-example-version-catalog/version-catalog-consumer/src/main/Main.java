6df8f6c971c968ddf3635d954085e4ac29ae1823
Guido Bacce <gbacce@gmail.com>
2025-07-31 12:41:06 -0400

    Delete gradle-examples directory

--- gradle-examples/gradle-example-version-catalog/version-catalog-consumer/src/main/Main.java
+++
@@ -1,9 +0,0 @@
-package com.jfrog.main;
-
-import org.apache.commons.lang3.StringUtils;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(StringUtils.isBlank(""));
-    }
-}
